5e01a9c55cc247f53981f119a22f206a8fa6463c
hridyansh <hridyansh68@gmail.com>
2018-09-01 18:40:05 +0530

    Testing no.of frames for video processing

---
+++ capture.py
@@ -0,0 +1,22 @@
+import cv2, time
+
+video = cv2.VideoCapture(0)
+a=0
+while True:
+    a=a+1
+    check, frame = video.read()
+
+    time.sleep(3)
+
+    cv2.imshow("Capturing",frame)
+    key = cv2.waitKey(1)
+
+    if key==ord('q'):
+        break
+
+
+video.release()
+
+cv2.destroyAllWindows()
+
+print(a)
